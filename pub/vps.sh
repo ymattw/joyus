b2605999bcb98e6b50c2eb4de81d04337efbc772
Matt Wang <ymattw@users.noreply.github.com>
2021-01-03 14:29:32 +0100

    vps.sh also works for Ubuntu 20.04.

--- pub/vps.sh
+++ pub/vps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Script to setup account on Ubuntu VPS. Verified versions:16.04, 18.04, 19.04.
+# Script to setup account on Ubuntu VPS. Verified versions:16.04, 18.04, 19.04,
+# 20.04.
 #
 # Example usage: curl -L joyus.org/pub/vps.sh | bash -s ymattw
 
